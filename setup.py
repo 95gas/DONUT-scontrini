ba571fd90ad50f312b28d1716f8e6771366f99ff
95gas <46025764+95gas@users.noreply.github.com>
2023-08-11 19:18:36 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read_version():
 
 
 def read_long_description():
-    path = os.path.join(ROOT, "README.md")
+    path = os.path.join(ROOT, "README.txt")
     with open(path, "r", encoding="utf-8") as f:
         text = f.read()
     return text
